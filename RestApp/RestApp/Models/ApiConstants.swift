256f0a42123f2d4a727fd52f9f95d7691aa0624f
FilovetsAlexandr <sohan.boxan@gmail.com>
2023-10-12 12:16:36 +0300

    add map for users

--- RestApp/RestApp/Models/ApiConstants.swift
+++ RestApp/RestApp/Models/ApiConstants.swift
@@ -14,13 +14,16 @@ struct ApiConstants {
     /// Users
     static let usersPath = serverPath + "users"
     static let usersURL = URL(string: usersPath)
-    
     /// Posts
-    
+    static let postsPath = serverPath + "posts"
+    static let postsURL = URL(string: postsPath)
     /// ToDo-s
-    
+    static let todosPath = serverPath + "todos"
+    static let todosURL = URL(string: todosPath)
     /// Alboms
-    
+    static let albomsPath = serverPath + "alboms"
+    static let albomsURL = URL(string: albomsPath)
     /// Photos
-    
+    static let photosPath = serverPath + "photos"
+    static let photosURL = URL(string: photosPath)
 }
